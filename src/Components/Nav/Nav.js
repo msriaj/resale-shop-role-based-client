ba0f016e3978ae5bd9e77da28d96b8dacc40615f
md-riaj11 <contact.riaj@gmail.com>
2022-11-28 05:03:28 +0600

    Overview Page Added

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -17,7 +17,7 @@ const Nav = () => {
         </Link>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
           {user?.uid && (
-            <Link to="/dashboard" className="mr-5 hover:text-gray-900">
+            <Link to="/dashboard/overview" className="mr-5 hover:text-gray-900">
               Dashboard
             </Link>
           )}
--- src/Pages/Overview/Overview.js
+++ src/Pages/Overview/Overview.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { useAuth } from "../../hooks/useAuth";
 
 const Overview = () => {
-  return <div>Overview</div>;
+  const { user } = useAuth();
+  return (
+    <div className="h-screen w-full p-12 text-gray-500">
+      <h1 className="text-2xl font-bold">Hello, {user?.displayName}</h1>
+      <p>Welcome To Dashboard</p>
+    </div>
+  );
 };
 
 export default Overview;
